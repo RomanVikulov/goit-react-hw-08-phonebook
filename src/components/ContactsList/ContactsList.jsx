edfc8e5ca779a00ba113a927576ee6656cd916eb
Roman <romanuch06.88@gmail.com>
2022-12-25 12:03:58 +0200

    Create ContactsList.jsx

---
+++ src/components/ContactsList/ContactsList.jsx
@@ -0,0 +1,34 @@
+import { useSelector } from 'react-redux';
+import { useGetContactsQuery } from 'redux/phonebook/operations';
+import { selectFilter } from 'redux/phonebook/selectors';
+import { Contact } from 'components/Contact/Contact';
+import { List, Notify } from './ContactsList.styled';
+
+export const ContactList = () => {
+  const {
+    data: contacts = [],
+    isFetching: isLoading,
+    isError,
+  } = useGetContactsQuery();
+  const filter = useSelector(selectFilter);
+  const getVisibleContacts = () => {
+    return contacts.filter(({ name }) => name.toLowerCase().includes(filter));
+  };
+  const visibleContacts = getVisibleContacts();
+
+  return (
+    <List>
+      {visibleContacts.length > 0 &&
+        visibleContacts.map(({ id, name, number }) => (
+          <Contact key={id} id={id} name={name} number={number} />
+        ))}
+
+      {isLoading && !isError && <Notify>Loading...</Notify>}
+      {isError && <Notify>{isError}</Notify>}
+
+      {visibleContacts.length === 0 && !isError && !isLoading && (
+        <Notify>No contacts</Notify>
+      )}
+    </List>
+  );
+};
