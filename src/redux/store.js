38ed05d0deb22d87b7bde201014ba067ee322cff
Roman <romanuch06.88@gmail.com>
2022-12-25 12:04:40 +0200

    Create store.js

---
+++ src/redux/store.js
@@ -0,0 +1,45 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { authReducer } from './auth/slice';
+import { phonebookApi } from './phonebook/operations';
+import { contactsReducer } from './phonebook/contactsSlice';
+import { filtersReducer } from './phonebook/filterSlice';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    [phonebookApi.reducerPath]: phonebookApi.reducer,
+    contacts: contactsReducer,
+    filter: filtersReducer,
+  },
+
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+    phonebookApi.middleware,
+  ],
+  devTools: process.env.NODE_ENV === 'development',
+});
+
+export const persistor = persistStore(store);
+setupListeners(store.dispatch);
