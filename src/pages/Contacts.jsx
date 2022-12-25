7f78d771be6a4e7dcc9eb1f380da1837f52e06cc
Roman <romanuch06.88@gmail.com>
2022-12-25 12:04:17 +0200

    Create Contacts.jsx

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,33 @@
+import { ContactForm } from '../components/ContactsForm/ContactsForm';
+import { ContactList } from '../components/ContactsList/ContactsList';
+import { Filter } from '../components/ContactsFilter/ContactsFilter';
+import {
+  Container,
+  Section,
+  Title,
+} from '../components/base styles/CommonStyle.styled';
+import { SEO } from 'utils/SEO';
+
+const Contacts = () => {
+  return (
+    <main>
+      <SEO title="Contacts" description="You can create your contacts list" />
+      <Container>
+        <Section>
+          <Title as="h1" hidden>
+            Phonebook
+          </Title>
+          <Title as="h2">Create New Contact</Title>
+          <ContactForm />
+        </Section>
+        <Section>
+          <Title as="h2">Contacts</Title>
+          <Filter />
+          <ContactList />
+        </Section>
+      </Container>
+    </main>
+  );
+};
+
+export default Contacts;
