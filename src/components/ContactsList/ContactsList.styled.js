2c0a4d381707185be300d63251d0053927354af6
Roman <romanuch06.88@gmail.com>
2022-12-25 12:04:00 +0200

    Create ContactsList.styled.js

---
+++ src/components/ContactsList/ContactsList.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const List = styled.ul`
+  width: calc(100vw - 40px);
+  @media screen and (min-width: 320px) {
+    max-width: 300px;
+  }
+  @media screen and (min-width: 768px) {
+    max-width: 400px;
+  }
+`;
+
+const Notify = styled.p`
+  margin-bottom: 15px;
+  font-weight: 500;
+`;
+export { List, Notify };
