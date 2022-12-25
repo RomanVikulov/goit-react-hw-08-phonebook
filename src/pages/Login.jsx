f703aaba13b7b1b9c07ed94329856d4ad25250d4
Roman <romanuch06.88@gmail.com>
2022-12-25 12:04:22 +0200

    Create Login.jsx

---
+++ src/pages/Login.jsx
@@ -0,0 +1,23 @@
+import {
+    Container,
+    Section,
+    Title,
+  } from 'components/base styles/CommonStyle.styled';
+  import { LoginForm } from 'components/LoginForm/LoginForm';
+  import { SEO } from 'utils/SEO';
+  
+  const Login = () => {
+    return (
+      <main>
+        <SEO title="Login" description="You can login to your account" />
+        <Container>
+          <Section>
+            <Title>Log in to your account</Title>
+            <LoginForm />
+          </Section>
+        </Container>
+      </main>
+    );
+  };
+  
+  export default Login;
