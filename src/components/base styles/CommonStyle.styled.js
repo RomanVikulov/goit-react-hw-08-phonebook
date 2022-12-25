a4b82ca4076105535da8fa069b776d9476c8c865
Roman <romanuch06.88@gmail.com>
2022-12-25 12:03:33 +0200

    Create CommonStyle.styled.js

---
+++ src/components/base styles/CommonStyle.styled.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: ${props => props.display || 'flex'};
+  flex-direction: ${props => props.flexDirection || 'column'};
+  align-items: ${props => props.alignItems || 'center'};
+  justify-content: c ${props => props.justifyContent || 'center'};
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 20px;
+  @media screen and (min-width: 768px) {
+    padding: 0 40px;
+    justify-content: space-evenly;
+    flex-direction: row;
+    align-items: flex-start;
+  }
+  @media screen and (min-width: 1280px) {
+    max-width: 1200px;
+  }
+`;
+
+const Section = styled.section`
+  margin: ${props => props.margin || '40px 0 0 0'};
+`;
+
+const Title = styled.h1`
+  margin: ${props => props.margin || '0 0 20px 0'};
+  text-align: ${props => props.position || 'start-left'};
+  font-size: ${props => props.size || '18px'};
+  font-weight: 700;
+  line-height: 1.33;
+  color: rgb(10 10 130);
+`;
+
+export { Container, Section, Title };
