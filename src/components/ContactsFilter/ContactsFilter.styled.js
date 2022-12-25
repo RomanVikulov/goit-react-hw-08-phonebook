3a637f2e3c3dcb9a0a67c8f33537248b5f2f4f67
Roman <romanuch06.88@gmail.com>
2022-12-25 12:03:54 +0200

    Create ContactsFilter.styled.js

---
+++ src/components/ContactsFilter/ContactsFilter.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Field, Label, Input } from '../base styles/Form.styled';
+
+const FieldFilter = styled(Field)``;
+const LabelFilter = styled(Label)``;
+const InputFilter = styled(Input)``;
+
+export { FieldFilter, LabelFilter, InputFilter };
