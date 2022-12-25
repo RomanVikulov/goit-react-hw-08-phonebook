0e768a981a12820f3bbf053e948b63b9adbabf48
Roman <romanuch06.88@gmail.com>
2022-12-25 12:04:09 +0200

    Create UserMenu.styled.js

---
+++ src/components/UserMenu/UserMenu.styled.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  @media screen and (min-width: 320px) {
+    gap: 10px;
+  }
+  @media screen and (min-width: 768px) {
+    gap: 15px;
+  }
+`;
+
+const Text = styled.p`
+  font-size: 14px;
+  font-weight: 500;
+  color: rgb(40 40 100);
+  @media screen and (min-width: 768px) {
+    font-size: 18px;
+  }
+`;
+
+const LogOutBtn = styled.button`
+  font-size: 14px;
+  font-weight: 500;
+  color: rgb(255 255 255);
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  @media screen and (min-width: 768px) {
+    font-size: 18px;
+  }
+  :hover,
+  :focus {
+    color: rgb(40 40 100);
+  }
+`;
+
+export { Box, Text, LogOutBtn };
