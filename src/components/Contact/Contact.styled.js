9295fa806c17556e736417a057e90750df5f81b7
Roman <romanuch06.88@gmail.com>
2022-12-25 12:03:45 +0200

    Create Contact.styled.js

---
+++ src/components/Contact/Contact.styled.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import { IconButton } from 'components/base styles/Buttons';
+
+const Item = styled.li`
+  display: flex;
+  gap: 10px;
+  align-items: flex-end;
+  align-content: center;
+  color: rgb(100 100 100);
+  &:not(:last-child) {
+    margin-bottom: 10px;
+  }
+  &:hover,
+  &:focus {
+    scale: 1.1;
+    padding: 5px;
+    border-radius: 5px;
+    border: 1px solid rgba(150, 150, 150, 0.7);
+    box-shadow: 3px 5px 10px -2px rgba(50, 150, 190, 0.5);
+  }
+`;
+
+const Name = styled.span`
+  margin-right: auto;
+  font-size: 14px;
+  font-weight: 500;
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+  }
+`;
+
+const Number = styled.span`
+  font-size: 12px;
+  font-weight: 400;
+  @media screen and (min-width: 768px) {
+    font-size: 14px;
+  }
+`;
+
+const IconBtn = styled(IconButton)``;
+
+export { Item, Name, Number, IconBtn };
