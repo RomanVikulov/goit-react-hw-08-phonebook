a9f0420de599c102b4ceed5c1c675878d930bef0
Roman <romanuch06.88@gmail.com>
2022-12-25 12:03:39 +0200

    Create GlobalStyle.js

---
+++ src/components/base styles/GlobalStyle.js
@@ -0,0 +1,26 @@
+import 'modern-normalize';
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+ 
+background: radial-gradient(circle, rgb(100, 200, 300) 0%, rgb(400,500,600) 100%);
+}
+  code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+  }
+  h1,h2,h3,h4,h5,h6,p {
+    margin: 0;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+`;
